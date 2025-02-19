4473982d1a9433f3cc585430d4ba633647800a59
danielcrubens <dcr.tecnologi@gmail.com>
2025-02-19 11:20:39 -0300

    refactor: refatorando home

--- pages/index.vue
+++ pages/index.vue
@@ -28,35 +28,12 @@
             Dias 15 a 17 de março | Das 18h às 21h | Online & Gratuito
           </p>
         </div>
-        <form class="w-full bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 md:max-w-[440px]">
-          <h2 class="font-heading font-semibold text-gray-200 text-xl">
-            Inscrição
-          </h2>
-          <div class="space-y-3">
-            <InputRoot>
-              <InputIcon>
-                <User :size="18" />
-              </InputIcon>
-              <InputField type="text" placeholder="Nome completo" />
-            </InputRoot>
-            <InputRoot>
-              <InputIcon>
-                <Mail :size="18" />
-              </InputIcon>
-              <InputField type="text" placeholder="E-mail" />
-            </InputRoot>
-          </div>
-          <Button type="submit">
-            Confirmar
-            <ArrowRight />
-          </Button>
-        </form>
+       <SubscriptionForm/>
       </div>
     </section>
 </template>
 
 <script setup lang="ts">
-import { ArrowRight, Radio, User, Mail } from 'lucide-vue-next'
-import Button from '@/components/atoms/Button/Button.vue'
-import { InputRoot, InputIcon, InputField } from '@/components/atoms/Input'
+import {Radio } from 'lucide-vue-next'
+import SubscriptionForm from '@/components/molecules/SubscriptionForm.vue'
 </script>
