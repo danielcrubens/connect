b2dac8d59d7af2cddf0e6a6cfa9de6d20e3c9fe8
danielcrubens <dcr.tecnologi@gmail.com>
2025-02-19 11:19:28 -0300

    feat: criando página invite

---
+++ pages/invite.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
+    <div class="flex flex-col gap-10 w-full max-w-[550px]">
+      <img src="@/assets/images/logo.svg" alt="devstage" width="108.5" height="30" />
+      <div class="space-y-2">
+        <h1 class="text-4xl font-semibold font-heading text-gray-100 leading-none">
+          Inscrição confirmada!
+        </h1>
+        <p class="text-gray-300">
+          Para entrar no evento, acesse o link enviado para seu e-mail.
+        </p>
+      </div>
+      <div class="space-y-6">
+        <div class="space-y-3">
+          <h2 class="text-gray-200 text-xl font-heading font-semibold leading-none">
+            Indique e ganhe
+          </h2>
+          <p class="text-gray-300">
+            Convide mais pessoas para o evento e concorra a prêmios
+            exclusivos! É só compartilhar o link abaixo e acompanhar as
+            inscrições:
+          </p>
+        </div>
+      </div>
+     <InviteLink/>
+     <Stats/>
+    </div>
+      <Raking/>
+  </section>
+</template>
+
+<script setup lang="ts">
+import Raking from '@/components/organisms/Ranking.vue'
+import Stats from '@/components/organisms/Stats.vue'
+import InviteLink from '@/components/organisms/InviteLink.vue'
+</script>
